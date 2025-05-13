526f58087779551429f37cc4d3648242049382b4
andrewpatto <andrew@patto.net>
2025-05-13 11:05:10 +1000

    Added init of basic logger

--- cloud-checksum/src/main.rs
+++ cloud-checksum/src/main.rs
@@ -1,8 +1,15 @@
+use tracing_subscriber::EnvFilter;
 use cloud_checksum::cli::Command;
 use cloud_checksum::error::Result;
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    // install global collector configured based on RUST_LOG env var.
+    let _ = tracing_subscriber::fmt()
+        .with_env_filter(EnvFilter::from_default_env())
+        .with_writer(std::io::stderr)
+        .try_init();
+    
     let args = Command::parse_args()?;
 
     args.execute().await?;
